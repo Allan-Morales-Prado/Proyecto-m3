c20c99c791a0bba7ff7ed3174855ca3b095b717c
Allan-Morales-Prado <allanmoralesprado@gmail.com>
2026-06-27 19:01:35 -0400

    Interacción básica con el usuario

---
+++ script.js
@@ -0,0 +1,5 @@
+// Mostrar por la consola del navegador un mensaje 
+console.log("Proyecto M3 - Paso 1");
+
+// Pedir nombre al usuario y mostrar una respuesta por pantalla
+alert("Hola, " + prompt("Ingresa tu nombre"));
