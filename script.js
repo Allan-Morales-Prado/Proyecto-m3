a33aa10355e0b6b1649acae387782f39cde8daf1
Allan-Morales-Prado <allanmoralesprado@gmail.com>
2026-06-27 19:59:36 -0400

    calculadora web

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-// Mostrar por la consola del navegador un mensaje 
-console.log("Proyecto M3 - Paso 1");
+// Variables numéricas ingresadas por el usuario
+const numero1 = parseInt(prompt("Ingrese un número"));
+const numero2 = parseInt(prompt("Ingrese otro número"));
 
-// Pedir nombre al usuario y mostrar una respuesta por pantalla
-alert("Hola, " + prompt("Ingresa tu nombre"));
+// Menú de opciones
+let opcion = prompt(`
+    Opciones disponibles:
+      1. Suma
+      2. Resta
+      3. Multiplicación
+      4. División
+      5. Módulo (resto de una división)
+
+    Ingrese el número correspondiente
+  `);
+
+// Control de flujo
+switch (opcion) {
+  case "1": 
+    alert(`${numero1} + ${numero2} = ${numero1 + numero2}`);
+    break;
+  case "2": 
+    alert(`${numero1} - ${numero2} = ${numero1 - numero2}`);
+    break;
+  case "3": 
+    alert(`${numero1} * ${numero2} = ${numero1 * numero2}`);
+    break;
+  case "4":
+    if (numero2 == 0)
+      alert("No se puede dividir por cero");
+    else
+      alert(`${numero1} / ${numero2} = ${numero1 / numero2}`);
+    break;
+  case "5": 
+    alert(`${numero1} % ${numero2} = ${numero1 % numero2}`);
+    break;
+  default:
+    alert("opción no encontrada");
+  
+}
